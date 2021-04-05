b0bcb1c9e013be497ba226ac414697c390aed54c
mzaini30 <muhzaini30@gmail.com>
2021-04-05 16:21:28 +0800

    upload

--- avalon/olah-sql.php
+++ avalon/olah-sql.php
@@ -1,5 +1,15 @@
 <?php 
 	$datanya = '';
+	if (!!$_POST) {
+		$sql = 'update excalibur_ringkasan_sql set semuaSql = "' . $_POST['data'] . '" where idDatabase = "'. $_GET['database'] .'"';
+		$db->prepare($sql)->execute();
+		$hapus = 'delete from excalibur_sql where idDatabase = "'. $_GET['database'] .'"';
+		$db->prepare($hapus)->execute();
+		$semuaPerintah = $_POST['data'];
+		$semuaPerintah = str_replace("[db]", 'kucing', $semuaPerintah);
+		$jadikanArray = explode("\n", $semuaPerintah);
+		var_dump($jadikanArray);
+	}
 	if (!!$_GET['database']) {
 		$sql = 'select * from excalibur_ringkasan_sql where idDatabase = "' . $_GET['database'] . '"';
 		$hasil = olah($sql, $db);
@@ -8,11 +18,11 @@
 ?>
 <form action="" method="post">
 	<div class="mb-3">
-		<textarea name="" id="" cols="30" rows="10" class="form-control elData"><?= $datanya ?></textarea>
+		<textarea id="" cols="30" rows="10" name="data" class="form-control elData"><?= $datanya ?></textarea>
 	</div>
 	<input type="submit" class="btn btn-success" value="Update">
 </form>
-
+<!-- 
 <script>
 	const elData = document.querySelector('.elData')
 	function tingginya(){
@@ -20,4 +30,4 @@
 	}
 	tingginya()
 	window.addEventListener('resize', tingginya)
-</script>
+</script> -->
