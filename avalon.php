5aba788d0e087bee2d74a3ffe3e23511bce81b60
mzaini30 <muhzaini30@gmail.com>
2021-04-05 12:16:30 +0800

    upload

--- avalon.php
+++ avalon.php
@@ -5,8 +5,31 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Olah Data</title>
+	<link rel="stylesheet" href="vendor/bootstrap.min.css">
 </head>
 <body>
-	
+	<div class="container py-3">
+		<h1 class="text-center">Excalibur</h1>
+		<hr>
+		<div class="row">
+			<div class="col-sm-2 mb-3">
+				<div class="list-group">
+					<a href="avalon.php" class="list-group-item list-group-item-action">Beranda</a>
+					<a href="?halaman=tambah-database" class="list-group-item list-group-item-action">Tambah Database</a>
+				</div>
+			</div>
+			<div class="col-sm mb-3">
+				<?php 
+					if(!$_GET['halaman']) {
+						include('halaman/index.php');
+					} else {
+						if($_GET['halaman'] == 'tambah-database') {
+							include('halaman/tambah-database.php');
+						}
+					}
+				?>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include('base.php') ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
