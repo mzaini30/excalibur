ed03c71014997b3c848728b5109daf6dafac7676
mzaini30 <muhzaini30@gmail.com>
2021-05-03 15:46:41 +0800

    upload

--- sql.php
+++ sql.php
@@ -14,18 +14,9 @@
 		$hasil = olah($sql, $db);
 		$perintahnya = $hasil[0]['perintah'];
 		foreach ($_POST as $key => $value) {
-			// echo json_encode($key);
 			$perintahnya = preg_replace('/\['.$key.'\]/', $db->quote($value), $perintahnya);
 		}
-		// echo json_encode($perintahnya);
-		// echo json_encode($_POST);
-		// $perintahnya = '"' . $hasil[0]['perintah'] . '"';
-		// $perintahnya = preg_replace("/\[([a-z0-9_]+)\]/i", '$_POST["$1"]', $perintahnya);
-		# $perintahnya = preg_replace("/\[([a-z0-9_]+)\]/i", '".$_POST["$1"]."', $perintahnya);
-		// $perintahnya = "insert into database_ca04a5e26b59240a3a022aa16ad3c0d0 (mau, makan) values ('".$_POST["pertama"]."', '".$_POST["kedua"]."')";
-		// $perintahnya = eval($perintahnya);
 		$jalankan = olah($perintahnya, $db);
-		// echo json_encode($perintahnya);
 		echo json_encode($jalankan);
 	}
 ?>
